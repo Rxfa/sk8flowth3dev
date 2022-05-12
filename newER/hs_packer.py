c17cde5b23d16de1afeb0cbfbf3e9c8ab7e388e3
a1c43my <sk8flowtheengineer@outlook.com>
2022-05-12 06:21:20 -0500

    restore

--- newER/hs_packer.py
+++ newER/hs_packer.py
@@ -1,8 +1,8 @@
 import socket
 import argparse
-import threading 
+import threading,time,base64
 
-cmd_line_in = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
+cmd_line_in = argparse.ArgumentParser(description = "This is the H&S Server")
 cmd_line_in.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = '0.0.0.0')
 cmd_line_in.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 2020)
 args = cmd_line_in.parse_args()
@@ -13,7 +13,7 @@ c2 = socket.socket()
 c2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 try: 
-	c2.bind((args.host, args.port))
+	c2.bind(("0.0.0.0", 2020))
 	c2.listen(5)
 except Exception as e:
 	raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
@@ -29,6 +29,7 @@ def on_new_client(client, connection):
 			break
 		if str(msg.decode()).__contains__('hs'): print(f"The client {connection[1]}said: {msg.decode()}");client.send('hs'.encode('utf-8'))
 		if msg.decode() == 'first': print(f"activating rack on {port}");client.send('activate'.encode())
+		else: time.sleep(1)
 	print(f"The client from ip: {ip}, and port: {port}, has gracefully diconnected!")
 	client.close()
 
@@ -39,6 +40,7 @@ while True:
 	except KeyboardInterrupt:
 		print(f"Gracefully shutting down the server!");raise SystemExit('you quit look slike')
 	except Exception as e:
-		print(f"Well I did not anticipate this: {e}")
+		#print(f"Well I did not anticipate this: {e}")
+		time.sleep(2)
 
 	c2.close()
